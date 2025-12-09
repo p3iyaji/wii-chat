9e0f43c25a5fc0597858dc9e404d35f87a4f9b5a
p3iyaji <p3.iyaji@gmail.com>
2025-12-09 10:25:32 +0000

    new commit

---
+++ public/build/assets/Spinner.vue_vue_type_script_setup_true_lang-BDXU_5Bt.js
@@ -0,0 +1,6 @@
+import{c as o}from"./index-tO9BhvBS.js";import{c as r}from"./createLucideIcon-Cq4THym-.js";import{B as c,D as n,d as a,j as t,i}from"./app-DadADKby.js";/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=r("LoaderCircleIcon",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]),f=c({__name:"Spinner",props:{class:{}},setup(s){const e=s;return(p,m)=>(i(),n(a(l),{role:"status","aria-label":"Loading",class:t(a(o)("size-4 animate-spin",e.class))},null,8,["class"]))}});export{f as _};
